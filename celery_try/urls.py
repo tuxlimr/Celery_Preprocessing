15f324d3ca13c3586102ec83b317523bb8663d6f
Ajay  sharma <itsecty.ajay@gmail.com>
2020-06-06 14:58:19 +0530

    removal of extra content

--- celery_try/urls.py
+++ celery_try/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path,include
-# from django.conf.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'celery_try.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    # url(r'^admin/', include(admin.site.urls)),
-    # url(r'^index/', include('testapp.urls')),
     path('admin/', admin.site.urls),
     path('', include('testapp.urls')),
 ]
